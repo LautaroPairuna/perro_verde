8ec029600a40cfd1fb577ec30173e0b491d2f3db
LautaroPairuna <lautaropairuna@gmail.com>
2025-05-09 16:42:14 -0300

    refactor: eliminar comentarios innecesarios en next.config.ts para mejorar la claridad del código

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,11 @@
 import { NextConfig } from 'next'
 import dotenv from 'dotenv'
 
-// Carga explícitamente las variables de entorno
 dotenv.config()
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
 
-  // Si sigues usando <Image> con URLs absolutas, conserva tu remotePatterns.
   images: {
     remotePatterns: [
       {
@@ -20,12 +18,27 @@ const nextConfig: NextConfig = {
     ],
   },
 
-  // Redirige /images/* al handler dinámico en src/app/api/disk-images/[...filePath]/route.ts
   async rewrites() {
     return [
       {
         source: '/images/:path*',
         destination: '/api/disk-images/:path*',
+      },
+    ]
+  },
+
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: '^perroverdepet\\.shop$', // redirige sin www → con www
+          },
+        ],
+        destination: 'https://www.perroverdepet.shop/:path*',
+        permanent: true,
       },
     ]
   },
