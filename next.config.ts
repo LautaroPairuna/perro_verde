4e34d5b3e19d6166b3aabeb8d9bc9e4deb956c58
LautaroPairuna <lautaropairuna@gmail.com>
2025-05-12 09:57:22 -0300

    refactor: reactivar la función de redirecciones en next.config.ts para mejorar la gestión de rutas

--- next.config.ts
+++ next.config.ts
@@ -27,21 +27,32 @@ const nextConfig: NextConfig = {
     ]
   },
 
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: '/:path*',
-  //       has: [
-  //         {
-  //           type: 'host',
-  //           value: '^perroverdepet\\.shop$', // redirige sin www → con www
-  //         },
-  //       ],
-  //       destination: 'https://www.perroverdepet.shop/:path*',
-  //       permanent: true,
-  //     },
-  //   ]
-  // },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: '^perroverdepet\\.shop$', // redirige sin www → con www
+          },
+        ],
+        destination: 'https://www.perroverdepet.shop/:path*',
+        permanent: true,
+      },
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: '^www\\.perroverdepets\\.shop$',
+          },
+        ],
+        destination: 'https://www.perroverdepet.shop/:path*',
+        permanent: true,
+      }
+    ]
+  },
 }
 
 export default nextConfig
