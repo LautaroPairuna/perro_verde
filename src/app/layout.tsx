5da04f69e5c0b90e753b00ecf67d13176be8e6eb
LautaroPairuna <lautaropairuna@gmail.com>
2025-06-30 20:40:08 -0300

    refactor: actualizar metadatos y mejorar la descripción del sitio

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,36 @@
-// src/app/layout.tsx  (SIN 'use client')
 import './globals.css'
 import { Metadata } from 'next'
 import { Toaster }  from 'react-hot-toast'
-import ClientShell  from '@/components/ClientShell'   // 👈 nuevo
+import ClientShell  from '@/components/ClientShell'   
+
+export const metadataBase = new URL('https://www.perroverdepet.shop')
 
 export const metadata: Metadata = {
   title: 'Perro Verde',
-  description: 'Descripción por defecto.',
+  description: 'Perro Verde - Tienda de mascotas ubicada en Salta, Argentina. Se encuentra en la Av. los Incas N°6 Local 2 ',
   icons: {
     icon: '/favicon.ico',       
     apple: '/favicon.ico',      
     shortcut: '/favicon.ico'    
-  }
+  },
+  keywords: [
+    'tienda de mascotas Salta',
+    'accesorios para mascotas',
+    'alimento para perros',
+    'comida para gatos',
+    'Perro Verde'
+  ],
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1
+    }
+  },
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
